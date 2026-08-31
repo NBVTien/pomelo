a23f1a7f8cf0e058d832efa016674996664628ff
toantran292 <tranthaitoan998@gmail.com>
2026-08-31 11:59:02 +0700

    release: v0.5.0

--- cmd/libpom/libpom.go
+++ cmd/libpom/libpom.go
@@ -37,7 +37,7 @@ var (
 	appDir string
 )
 
-const appVersion = "0.4.3"
+const appVersion = "0.5.0"
 
 //export PomInit
 func PomInit(cfgPath *C.char) *C.char {
--- cmd/pom/root.go
+++ cmd/pom/root.go
@@ -12,7 +12,7 @@ import (
 	"github.com/pomelohq/pomelo/internal/services"
 )
 
-const version = "0.4.3"
+const version = "0.5.0"
 
 var (
 	appConfig  *config.Config
